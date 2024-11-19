62e542cf1758d1966c944e3555cbaec308406b19
Matthew Ko <mattystar1000@gmail.com>
2024-11-19 12:13:24 -0500

    first commit

---
+++ template_final_project-master/src/Model_b.py
@@ -0,0 +1,7 @@
+class fire:
+    def __init__(self,x,y,speed, type,img_file):
+        self.x=x+speed
+        self.y=y+speed
+        self.speed=speed
+        self.type=0
+        self.img_file=img_file
