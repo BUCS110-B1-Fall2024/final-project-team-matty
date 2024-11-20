29766a9dbb49e1eefea819769a513f8e4cad7601
Matthew Ko <mattystar1000@gmail.com>
2024-11-19 21:49:58 -0500

    1ST commit

--- template_final_project-master/src/sample_controller.py
+++ template_final_project-master/src/sample_controller.py
@@ -1,11 +1,31 @@
+import pygame
+import Model_a
+import Model_b
 
 class Controller:
   
   def __init__(self):
+    pygame.init()
+    self.screen=pygame.display.set_mode()
+    size_list = pygame.display.get_window_size(width, height)
+    width = size_list[0]
+    height = size_list[1]
+    self.char = []
+                           
     #setup pygame data
+    #sets the size of the screen, and sets up which character to use
     
   def mainloop(self):
+    while True:
+      for event in pygame.event.get():
+        if event.type == pygame.CONTROLLER_BUTTON_DPAD_UP:
+          up=True
+        
+          
+          
+        
     #select state loop
+    #setting up eventing for the controls and collision for objects
     
   
   ### below are some sample loop states ###
@@ -13,6 +33,7 @@ class Controller:
   def menuloop(self):
     
       #event loop
+      #main menu where you select character, difficulty, click to play or exit
 
       #update data
 
@@ -20,6 +41,7 @@ class Controller:
       
   def gameloop(self):
       #event loop
+      #starts the game events so you play the game
 
       #update data
 
@@ -27,6 +49,7 @@ class Controller:
     
   def gameoverloop(self):
       #event loop
+      #stops gameplay and tells you the time, and if you want to retry or go back to menu
 
       #update data
 
