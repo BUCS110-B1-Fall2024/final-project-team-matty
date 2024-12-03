09e6c6abfc74645e04c0631e4e6ada7381cea984
Matthew Ko <mattystar1000@gmail.com>
2024-12-02 23:56:41 -0500

    2nd commit

--- template_final_project-master/src/sample_controller.py
+++ template_final_project-master/src/sample_controller.py
@@ -1,56 +1,100 @@
 import pygame
-import Model_a
-import Model_b
+import pygame_menu
+
+
+width = 500
+height = 500
+bg_color = "black" 
 
 class Controller:
-  
-  def __init__(self):
-    pygame.init()
-    self.screen=pygame.display.set_mode()
-    size_list = pygame.display.get_window_size(width, height)
-    width = size_list[0]
-    height = size_list[1]
-    self.char = []
-                           
-    #setup pygame data
-    #sets the size of the screen, and sets up which character to use
-    
-  def mainloop(self):
-    while True:
-      for event in pygame.event.get():
-        if event.type == pygame.CONTROLLER_BUTTON_DPAD_UP:
-          up=True
+    def __init__ (self):
+        pygame.init()
         
-          
-          
+        self.screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Fireball_Survival")
+        self.state = "Start"
+        self.menu = pygame_menu.Menu("Fireball Survival", width, height)
+    
+    def start_game(self):
+        self.state = "Game"  
+    
+    def mainloop(self):
+        while True:
+            if self.state == "Start":
+                self.start_loop()
+            elif self.state == "Game":
+                self.gameloop()
+            elif self.state == "End":
+                self.endloop()
+
+    def start_loop(self):
+        self.menu.clear() 
+        self.menu = pygame_menu.Menu("Fireball Survival", width, height)
+        self.menu.add.label("Begin Game?", font_size=16)
+        self.menu.add.button("Start", self.gameloop) 
+        self.menu.add.button("Quit", self.quit_game)  
+        self.menu.mainloop(self.screen) 
+
+    def gameloop(self):
+
+        char_x = 0
+        char_y = 0
+        char_width=25 
+        char_height = 25
+        char_speed=0.25
+        char_color="Blue"
+
+        fire_x= 50 
+        fire_y = 25
+        fire_width=25
+        fire_height =25
+        fire_speed=0.25
+        fire_color="Red"
+
+        running = True
+        while running:
+            self.screen.fill(bg_color)  
+            
+            
+            char = pygame.Rect(char_x, char_y, char_width, char_height)
+            pygame.draw.rect(self.screen, char_color, char)
+            fire = pygame.Rect(fire_x, fire_y, fire_width, fire_height)
+            pygame.draw.rect(self.screen, fire_color, fire)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if char.colliderect (fire):
+                  print('touch')
+             
+
+            keys = pygame.key.get_pressed() 
+            if keys[pygame.K_LEFT] and char_x >= 0:
+              char_x-= char_speed
+            if keys[pygame.K_RIGHT] and char_x <= 475:
+              char_x+= char_speed        
+            if keys[pygame.K_DOWN] and char_y <= 475:
+              char_y+= char_speed
+            if keys[pygame.K_UP] and char_y>=0:
+              char_y-= char_speed          
+            if keys[pygame.K_ESCAPE]:
+              self.endloop()
         
-    #select state loop
-    #setting up eventing for the controls and collision for objects
-    
-  
-  ### below are some sample loop states ###
+            pygame.display.update()
 
-  def menuloop(self):
-    
-      #event loop
-      #main menu where you select character, difficulty, click to play or exit
+        self.state = "End"  
 
-      #update data
+    def endloop(self):
+        self.menu.clear()  
+        self.menu.add.label("You lost!", font_size=16)
+        self.menu.add.button("Back to menu", self.start_loop)
+        self.menu.draw(self.screen)
+        pygame.display.flip()
 
-      #redraw
-      
-  def gameloop(self):
-      #event loop
-      #starts the game events so you play the game
+    def quit_game(self):
+        pygame.quit()
+        exit()  
 
-      #update data
-
-      #redraw
-    
-  def gameoverloop(self):
-      #event loop
-      #stops gameplay and tells you the time, and if you want to retry or go back to menu
-
-      #update data
-
-      #redraw
+if __name__ == '__main__':
+    controller = Controller()
+    controller.mainloop()
