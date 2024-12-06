b81f95dd06666b5e5cc04ea3e6f9e6bb899899c3
Matthew Ko <mattystar1000@gmail.com>
2024-12-05 23:42:37 -0500

    final commit

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -5,12 +5,11 @@ from src.sample_controller import controller
 def main():
     pygame.init()
     control= controller()
-    #Create an instance on your controller object
     control.mainloop()
 
-    #Call your mainloop
 
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
+
+    
 
 # https://codefather.tech/blog/if-name-main-python/
 if __name__ == '__main__':
