6b6727e030ef57471e40630ea0b6d51ed26a1703
Matthew Ko <mattystar1000@gmail.com>
2024-12-05 23:41:52 -0500

    final commit

--- template_final_project-master/src/sample_controller.py
+++ template_final_project-master/src/sample_controller.py
@@ -8,10 +8,11 @@ width = 500
 height = 500
 bg_color = (0,0,0) 
 
-
 class controller:
     def __init__ (self):
         pygame.init()
+        
+        
         
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Fireball_Survival")
@@ -20,7 +21,15 @@ class controller:
     
     def start_game(self):
         self.state = "Game"  
-    
+        
+    def endloop(self):
+        self.menu = pygame_menu.Menu("Fireball Survival", width, height)
+        self.menu.add.label("Blue got hit!", font_size=16)
+        self.menu.add.button("Retry", self.gameloop) 
+        self.menu.add.button("Quit", self.quit_game)  
+        self.menu.mainloop(self.screen) 
+        
+        
     def mainloop(self):
         while True:
             if self.state == "Start":
@@ -38,56 +47,64 @@ class controller:
         self.menu.add.button("Quit", self.quit_game)  
         self.menu.mainloop(self.screen) 
 
-
     def gameloop(self):
         running = True
+        person=player()
+        enemy=fire()
+
         while running:
-            self.screen.fill(bg_color)  
-            person=player()
-            enemy=fire()
-            
+          self.screen.fill(bg_color)  
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              running = False
 
 
-            gamer = pygame.Rect(person.x, person.y, person.width, person.height)
-            pygame.draw.rect(self.screen, person.color, gamer)
-            fireball = pygame.Rect(enemy.x, enemy.y, enemy.width, enemy.height)
-            pygame.draw.rect(self.screen, enemy.color, fireball)
-
-
-
-            keys = pygame.key.get_pressed() 
-            for event in pygame.event.get():
-              if event.type == pygame.KEYDOWN:
-                if keys[pygame.K_LEFT] and person.x >= 0:
-                  person.x -= person.speed
-            if keys[pygame.K_RIGHT] and person.x <= 475:
-              person.x += person.speed
-            if keys[pygame.K_DOWN] and person.y <= 475:
-              person.y -= person.speed
-            if keys[pygame.K_UP] and person.y>=0:
-              person.y += person.speed         
-            if keys[pygame.K_ESCAPE]:
+          
+          keys = pygame.key.get_pressed() 
+          if keys[pygame.K_LEFT] and person.x > 0:
+            person.x -= person.speed
+          if keys[pygame.K_RIGHT] and person.x < width - person.width:
+            person.x += person.speed
+          if keys[pygame.K_UP] and person.y > 0:
+            person.y -= person.speed
+          if keys[pygame.K_DOWN] and person.y < height - person.height: 
+            person.y += person.speed
+          
+          if keys[pygame.K_a] and enemy.x > 0:
+            enemy.x -= enemy.speed
+          if keys[pygame.K_d] and enemy.x < width - enemy.width:
+            enemy.x += enemy.speed
+          if keys[pygame.K_w] and enemy.y > 0:
+            enemy.y -= enemy.speed
+          if keys[pygame.K_s] and enemy.y < height - enemy.height: 
+            enemy.y += enemy.speed
+          if keys[pygame.K_ESCAPE]:
               self.endloop()
         
-            pygame.display.update()
 
+            
+          fireball = pygame.Rect(enemy.x, enemy.y, enemy.width, enemy.height)
+          pygame.draw.rect(self.screen, enemy.color, fireball)
+          
+          gamer = pygame.Rect(person.x, person.y, person.width, person.height)
+          pygame.draw.rect(self.screen, person.color, gamer)
+          pygame.display.update() 
 
-             
+          
+          if gamer.colliderect(fireball):
+              self.state = "End"
+              running = False
 
-    def endloop(self):
-        self.menu.clear()  
-        self.menu = pygame_menu.Menu("Game Over", width, height) 
-        self.menu.add.label("You lost!", font_size=16)
-        self.menu.add.button("Back to menu", self.start_loop)
-        self.menu.draw(self.screen)
-        pygame.display.flip()
+        if self.state == "End":
+          self.endloop()
+         
+
         for event in pygame.event.get():
           if event.type == pygame.QUIT:
             running = False
             if running == False:
                   pygame.quit()
-            if gamer.colliderect(fireball):
-                  print('touch')
+
              
     def quit_game(self):
         pygame.quit()
